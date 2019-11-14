5e82d0fcb277b1f7de49569ccde477eebc1b7176
Jaren Jose De Hitta <jarendehitta@gmail.com>
2019-11-13 21:33:05 -1000

    difficult

--- DeHitta_Jaren_Assignment1/server.js
+++ DeHitta_Jaren_Assignment1/server.js
@@ -1,9 +1,10 @@
-const querystring = require('querystring');
+const querystring = require('querystring'); //require that the server responds to any errors
 
-var express = require('express');
+var express = require('express'); //express package
 var myParser = require("body-parser");
-var products = require("./public/product.js");
+var products = require("./public/product.js"); //take data from products.js in the public folder
 
+//borrowed code from Lab13
 var app = express();
 app.all('*', function (request, response, next) {
    console.log(request.method + ' to ' + request.path);
@@ -11,44 +12,47 @@ app.all('*', function (request, response, next) {
 });
 
 app.use(myParser.urlencoded({ extended: true }));
-//intercept purchase submission form, if good give an invoice, otherwise send back to order page
+
+//parially borrowed code from Assignment1 example
+// intercept purchase submission form, if good give an invoice, otherwise send back to order page
 app.get("/process_page", function (request, response) {
-   //check if quantity data is valid
-   //look up request.query
+   // check if quantity data is valid
    params = request.query;
    console.log(params);
    if (typeof params['purchase_submit'] != 'undefined') {
-      has_errors = false; // assume quantities are valid from the start
-      total_qty = 0; // need to check if something was selected so we will look if the total > 0
-      for (i = 0; i < products.length; i++) {
-         if (typeof params[`quantity${i}`] != 'undefined') {
+      has_errors = false; //assume quantities are valid from the start
+      total_qty = 0; //need to check if something was selected so we will look if the total > 0
+      for (i = 0; i < products.length; i++) { //checking each of the products in the array
+         if (typeof params[`quantity${i}`] != 'undefined') {  //if not undefined then move on to the next if statement
             a_qty = params[`quantity${i}`];
             total_qty += a_qty;
             if (!isNonNegInt(a_qty)) {
-               has_errors = true; // oops, invalid quantity
+               has_errors = true; //oops, invalid quantity
             }
          }
       }
+      //request to look at query list/data
       qstr = querystring.stringify(request.query);
-      // Now respond to errors or redirect to invoice if all is ok
+      //now respond to errors or redirect to invoice if all is ok
       if (has_errors || total_qty == 0) {
          //if quantity data is not valid, send them back to product display
          qstr = querystring.stringify(request.query);
-         response.redirect("product_display.html?" + qstr);
-      } else { // all good to go!
+         response.redirect("product_display.html?" + qstr); 
+         //if quantity data is valid, send an invoice
+      } else { //all good to go!
          response.redirect("invoice.html?" + qstr);
       }
    }
 });
-//if quantity data valid, send them to the invoice
 
-
-
-
-
+// if quantity data valid, send them to the invoice
+// borrowed code from Lab13
 app.use(express.static('./public'));
 app.listen(8080, () => console.log(`listening on port 8080`));
 
+
+// checking that data is valid
+// borrowed code from Lab13/Assigment1
 function isNonNegInt(q, returnErrors = false) {
    errors = []; // assume no errors at first
    if (q == "") { q = 0; }
@@ -57,14 +61,3 @@ function isNonNegInt(q, returnErrors = false) {
    if (parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
    return returnErrors ? errors : (errors.length == 0);
 }
-
-/*
-function displayPurchase(POST, response) {
-   q = POST['quantity_textbox'];
-   if (isNonNegInt(q)) {
-      response.send(`Thank you for purchasing ${q} things!`);
-   } else {
-      response.send(`${q} is not a quantity! Press the back button and try again.`);
-   }
-}
-*/
